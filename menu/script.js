add6f40a899ac5455c9b194bf32c6d27dda448a4
Nico4652 <nvega6511@gmail.com>
2025-03-21 02:37:36 -0300

    Add files via upload

---
+++ menu/script.js
@@ -0,0 +1,24 @@
+var swiper = new Swiper(".mySwiper-1", {
+    slidesPerView:1,
+    spaceBetween: 30,
+    loop:true,
+    pagination: {
+        el:".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl:".swiper-button-next",
+        prevEl:".swiper-button-prev",
+    }
+    
+});
+let tabInputs = document.querySelectorAll(".tabInput");
+
+tabInputs.forEach(function(input) {
+
+    input.addEventListener('change', function() {
+        let id = input.ariaValueMax;
+        let thisSwiper = document.getElementById('swiper' + id);
+        thisSwiper.swiper.update();
+    })
+});
